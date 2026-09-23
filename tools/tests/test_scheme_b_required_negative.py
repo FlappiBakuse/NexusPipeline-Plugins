2abfbed451ee791fa8053764dbb8bd91a48167e0
FlappiBakuse <111492950+FlappiBakuse@users.noreply.github.com>
2026-09-23 20:48:26 +0800

    test: 移除 Plugins 必需检查负向探针

--- tools/tests/test_scheme_b_required_negative.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class SchemeBRequiredNegativeProbe(unittest.TestCase):
-    def test_required_check_rejects_a_real_failure(self):
-        self.fail("Scheme B migration probe: this failure must block Plugins / Required")
